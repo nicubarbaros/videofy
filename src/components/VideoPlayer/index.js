ee8711326abb88d89e01bf3adb1cbd888989c7e1
Barbaros Nicolae <barbarosnico@MacBook-Pro.local>
2017-07-10 22:11:49 +0300

    style video like facbook alike

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -4,7 +4,7 @@ import "video.js/dist/video-js.css";
 
 export default class VideoPlayer extends React.Component {
   componentDidMount() {
-    // instantiate video.js
+    // this.props.sources[0].src = 'https://vjs.zencdn.net/v/oceans.mp4'
     this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
       console.log('onPlayerReady', this)
     });
@@ -16,18 +16,12 @@ export default class VideoPlayer extends React.Component {
       this.player.dispose()
     }
   }
-
-  // wrap the player in a div with a `data-vjs-player` attribute
-  // so videojs won't create additional wrapper in the DOM
-  // see https://github.com/videojs/video.js/pull/3856
+  
   render() {
     return (
       <div data-vjs-player>
         <video ref={ node => this.videoNode = node } className="video-js vjs-default-skin"></video>
       </div>
-
-
-
     )
   }
 }
